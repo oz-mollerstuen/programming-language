6b2aca46c57ff702e421db24e6215dd1cabff238
Lucas Mollerstuen <mollerstuen.works@gmail.com>
2022-10-21 10:21:24 -0700

    WIP,break: redo html, begin js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", function() {
+  const form = document.getElementById("lang");
+  form.addEventListener("submit", endResults);
+  console.log("submit");
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
