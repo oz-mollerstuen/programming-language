e7225d83a4511b49d1a9542627b4fd0bf4cced82
Lucas Mollerstuen <mollerstuen.works@gmail.com>
2022-10-21 14:56:51 -0700

    add more dialogue, add pictures to answers

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", function() {
-});
+
  function prevent(event) {
   event.preventDefault();
  }
-  
+});
 function math() {
   let val1score = 0;
   let val2score = 0;
@@ -29,10 +29,12 @@ function math() {
   }
 
   let maxscore = Math.max(val1score,val2score,val3score,val4score);
-  var answerbox = document.getElementById('answer');
+  let answerbox = document.getElementById('answer');
+  let triviabox = document.getElementById("trivia")
   if (val1score === maxscore) { 
     answerbox.innerHTML = "You should learn Python! Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis. Python is a general-purpose language, meaning it can be used to create a variety of different programs and isn’t specialized for any specific problems. This versatility, along with its beginner-friendliness, has made it one of the most-used programming languages today. A survey conducted by industry analyst firm RedMonk found that it was the second-most popular programming language among developers in 2021.";
-    trivia.innerHTML = "Did you know? The name Python comes from Monty Python. When Guido van Rossum was creating Python, he was also reading the scripts from BBC’s Monty Python’s Flying Circus. He thought the name Python was appropriately short and slightly mysterious.";
+    triviabox.innerHTML = "Did you know? The name Python comes from Monty Python. When Guido van Rossum was creating Python, he was also reading the scripts from BBC’s Monty Python’s Flying Circus. He thought the name Python was appropriately short and slightly mysterious.";
+    document.getElementById("montyp").removeAttribute("class");
   }
   if (val2score === maxscore) { 
     answerbox.innerHTML = "C#";
@@ -47,6 +49,9 @@ function math() {
 function redo() {
   let answerbox = document.getElementById('answer');
   answerbox.innerHTML = "Who's excited to see?";
+  let triviabox = document.getElementById('trivia');
+  triviabox.innerHTML = "Because I know I am";
+  document.getElementById("montyp").removeAttribute("class");
 }
 
 
